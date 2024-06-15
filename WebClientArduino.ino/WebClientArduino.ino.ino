c4bb6330465b19d6665bfceb4dc97cde624d7bf2
Александр <71236657+AlexaFedS@users.noreply.github.com>
2024-06-15 20:36:40 +0300

    Update WebClientArduino.ino.ino

--- WebClientArduino.ino/WebClientArduino.ino.ino
+++ WebClientArduino.ino/WebClientArduino.ino.ino
@@ -28,7 +28,7 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-IPAddress server(192,168,31,170);  // numeric IP for Google (no DNS)
+IPAddress server(192,168,//,//);  // your IP address (no DNS, DHCP)
 // server[] = "www.google.com";    // name address for Google (using DNS)
 
 // Set the static IP address to use if the DHCP fails to assign
